e5804e77fd15f47817923a4860c8777ce7fb4e8a
mephistofel <mephistofel66613@gmail.com>
2020-09-24 11:23:13 +0300

    create header & footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/components/header/header.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
@@ -16,3 +20,6 @@ import { AppComponent } from './app.component';
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+// ng build --prod --base-href "https://MephCat.github.io/questions"
+// angular-cli-ghpages
+// ngh --dir=dist/questions
