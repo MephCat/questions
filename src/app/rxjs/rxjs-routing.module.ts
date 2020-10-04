9162564a6f8f5ba231d9414af0c4bbc1d8722c77
mephistofel <mephistofel66613@gmail.com>
2020-10-04 14:21:24 +0300

    create js => destructuring, spread, localStorage

--- src/app/rxjs/rxjs-routing.module.ts
+++ src/app/rxjs/rxjs-routing.module.ts
@@ -2,9 +2,22 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { RxjsComponent } from './rxjs.component';
+import {AboutPageComponent} from './about-page/about-page.component';
+import {ObservablePageComponent} from './observable-page/observable-page.component';
 
 const routes: Routes = [
-  { path: '', component: RxjsComponent }
+  {
+    path: '',
+    component: RxjsComponent
+  },
+  {
+    path: 'about',
+    component: AboutPageComponent
+  },
+  {
+    path: 'observable',
+    component: ObservablePageComponent
+  }
 ];
 
 @NgModule({
--- src/app/rxjs/rxjs.module.ts
+++ src/app/rxjs/rxjs.module.ts
@@ -3,10 +3,16 @@ import { CommonModule } from '@angular/common';
 
 import { RxjsRoutingModule } from './rxjs-routing.module';
 import { RxjsComponent } from './rxjs.component';
+import { AboutPageComponent } from './about-page/about-page.component';
+import { ObservablePageComponent } from './observable-page/observable-page.component';
 
 
 @NgModule({
-  declarations: [RxjsComponent],
+  declarations: [
+    RxjsComponent,
+    AboutPageComponent,
+    ObservablePageComponent
+  ],
   imports: [
     CommonModule,
     RxjsRoutingModule
